32140bff889b5a6ea5212d7952acd0e717ce8eb3
Renato Siqueira <renatoelysiqueira@gmail.com>
2019-08-13 01:28:41 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const port = process.env.PORT
+const mongo = process.env.MONGODB
+
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Home')
+})
+
+mongoose
+    .connect(mongo, { useMongoClient: true })
+    .then(() => {
+        app.listen(port, () => console.log('Server Started on ' + port))
+    })
+    .catch(e => {
+        console.log(e)
+    })
