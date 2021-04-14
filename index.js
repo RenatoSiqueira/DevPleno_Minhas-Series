30022105dc9afbc5cf9809334b0d35846373fb9a
Renato Siqueira <renatoelysiqueira@gmail.com>
2021-04-14 11:27:52 -0300

    forcing

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
